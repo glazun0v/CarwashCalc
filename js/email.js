a73b09f8d64bb8af5acaa2c516ebe5707d508d6b
ZeroCool <g0s@ya.ru>
2019-12-08 17:43:26 +0300

    release v3

--- js/email.js
+++ js/email.js
@@ -1,27 +1,50 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-	$("#form").submit(function() {
+	// Проверка пустых строк
+	$("input[name=send]").click(function () {
+		if ($.trim($("[id='gosnum']").val()) == '') {
+			ohSnap('Введите госномер', {
+				color: 'red',
+				'duration': '2000'
+			});
+			return false;
+		} else if ($.trim($("[id='phone']").val()) == '') {
+			ohSnap('Введите телефон', {
+				color: 'red',
+				'duration': '2000'
+			});
+			return false;
+		} else if ($.trim($("[name='date']").val()) == '') {
+			ohSnap('Выберите дату', {
+				color: 'red',
+				'duration': '2000'
+			});
+			return false;
+		} else if ($("[name='checkbox-agree']").prop('checked') == false) {
+			/*$("#agree").css('border', '2px solid red');*/
+			ohSnap('Дайте своё согласие на обработку данных', {
+				color: 'red',
+				'duration': '2000'
+			});
+			return false;
+		}
+		
+	});
+
+	$("#form").submit(function () {
 		$.ajax({
 			type: "POST",
 			url: "js/email.php",
 			data: $(this).serialize()
-		}).done(function() {
+		}).done(function () {
 			$(this).find("input").val("");
-			/*alert("Спасибо за заявку! Скоро мы с вами свяжемся.");*/
-			ohSnap('Спасибо за заявку! Ждем Вас на мойке.', {color: 'green', 'duration':'5000'});	
+			ohSnap('Спасибо за заявку! Ждем Вас на мойке.', {
+				color: 'green',
+				'duration': '5000'
+			});
 			$("#form").trigger("reset");
 		});
 		return false;
 	});
 
-	// Согласие на обработку данных
-	$("input[name=send]").click(function() {
-		if ($("[name='checkbox-agree']").prop('checked') == false) { 
-			/*alert('Дайте своё согласие на обработку данных!');*/
-			/*$("#agree").css('border', '2px solid red');*/
-			ohSnap('Дайте своё согласие на обработку данных!', {color: 'red', 'duration':'2000'});		
-			return false;
-		}		
-	});
-	
 });
